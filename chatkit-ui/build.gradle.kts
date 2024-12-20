5a4964b3550b3f4d03340ad0311281586991b002
yiyikeji <wenhao.yang@yiartkeji.com>
2024-12-20 15:35:32 +0800

    1、打包配置

--- chatkit-ui/build.gradle.kts
+++ chatkit-ui/build.gradle.kts
@@ -82,7 +82,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.ywhdream"
                 artifactId = "im"
-                version = "1.0"
+                version = "1.0.1"
             }
         }
     }
