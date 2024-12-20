4e12acd1f30b2bfad7decb1c575d82394096c42d
yiyikeji <wenhao.yang@yiartkeji.com>
2024-12-20 18:10:23 +0800

    1、打包配置

--- chatkit-ui/build.gradle.kts
+++ chatkit-ui/build.gradle.kts
@@ -38,8 +38,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     sourceSets["main"].res.srcDirs("src/main/res", "src/main/res-fun", "src/main/res-normal")
